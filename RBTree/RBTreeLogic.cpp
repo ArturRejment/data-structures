e1dc6407d3335fc6d6fc28b58c3158d7f559e972
Artur Rejment <arturrejment1@gmail.com>
2021-04-11 13:30:43 +0200

    Starting experiment on rbtree

--- RBTree/RBTreeLogic.cpp
+++ RBTree/RBTreeLogic.cpp
@@ -49,3 +49,54 @@ void fillTreeWithRandomData(RBTree *tree)
         tree->insertElement(value);
     }
 }
+
+void fillTreeForExperiment(RBTree *tree, int size)
+{
+    int value;
+    if (!tree->isEmpty())
+    {
+        deleteTree(tree);
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        value = rand() % 100000;
+        tree->insertElement(value);
+    }
+}
+
+void treeExperiment(RBTree *tree)
+{
+    srand(time(NULL));
+    int size, value;
+    float time;
+
+    cout << "Please enter the size of the red-black tree: " << flush;
+    cin >> size;
+
+    //! Insert element
+    time = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        fillTreeForExperiment(tree, size);
+        value = rand() % 100000;
+        Timer timer;
+        tree->insertElement(value);
+        time += timer.getTime().count() * 1000.0f;
+    }
+    cout << "Inserting element to the red-black tree took on average: " << time / 100 << " ms\n";
+
+    //! Delete element
+    RBNode *node;
+    time = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        fillTreeForExperiment(tree, size);
+        value = rand() % 100000;
+        node = tree->find(value);
+        Timer timer;
+        tree->deleteElement(node);
+        time += timer.getTime().count() * 1000.0f;
+    }
+    cout << "Deleting element form the red-black tree took on average: " << time / 100 << " ms\n";
+}
